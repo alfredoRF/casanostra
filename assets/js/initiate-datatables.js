669e6d6c3b0026ac8f8ac926d32f02f7f69bb40e
Alfredo Rosales <rosalesfloresalfredo@gmail.com>
2024-01-03 10:38:28 -0600

    codigo de inicio

---
+++ assets/js/initiate-datatables.js
@@ -0,0 +1,11 @@
+// Initiate datatables in roles, tables, users page
+(function() {
+    'use strict';
+    $('#dataTables-example').DataTable({
+        responsive: true,
+        pageLength: 20,
+        lengthChange: false,
+        searching: true,
+        ordering: true
+    });
+})();
